b5f2b8acd56ca8be149b93cd6f84cdf62206ef53
Burke, Louis <Louis.Burke@hmhco.com>
2017-09-14 12:38:16 +0100

    Cleanup

--- matrix.py
+++ matrix.py
@@ -1,21 +1,26 @@
 from copy import deepcopy
+from datetime import datetime
 from json import load, dumps
 from random import choice
 from time import sleep
 from uuid import uuid4
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 
-with open("/Users/burkel/matrix.json") as matrix_json:
+with open("./matrix.json") as matrix_json:
     JSON=load(matrix_json)
 
 while True:
     temp_json=deepcopy(JSON)
-    temp_json["data"][0]["data"]["@id"] = str(uuid4())
-    temp_json["data"][0]["data"]["actor"]["extensions"]["orgId"] = str(uuid4())
-    temp_json["data"][0]["data"]["actor"]["extensions"]["orgRefId"] = str(uuid4())
-    delete = str(choice(list(temp_json["data"][0]["data"]["object"])))
-    del temp_json["data"][0]["data"]["object"][delete]
-    print("New event payload ==========================================>")
+    temp_json["data"]["@id"] = str(uuid4())
+    temp_json["data"]["actor"]["extensions"]["orgId"] = str(uuid4())
+    temp_json["data"]["actor"]["extensions"]["orgRefId"] = str(uuid4())
+    temp_json["data"]["eventTime"] = datetime.now().isoformat()
+    temp_json["sendTime"] = datetime.now().isoformat()
+    delete = str(choice(list(temp_json["data"]["object"])))
+    del temp_json["data"]["object"][delete]
+
+    print(RED + "New event payload ==========================================>")
     print(GREEN + dumps(temp_json, indent=4, sort_keys=True))
-    sleep(0.5)
+    sleep(0.25)
